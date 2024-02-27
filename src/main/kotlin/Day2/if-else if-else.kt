0ba1627e5658eccc31cebe57a1d9a0cbdbf33630
Umaq <feratj01@gmail.com>
2024-02-27 16:35:44 +0300

    Day 2 - AgeControl

---
+++ src/main/kotlin/Day2/if-else if-else.kt
@@ -0,0 +1,11 @@
+package Day2
+
+fun main(){
+    println("Enter your age as a whole number:")
+    val age = readln().toInt()
+    if (age in 18..39) {
+        println("You can enter the club.")
+    } else  {
+        println("You cannot go into the club, please go home.")
+    }
+}
